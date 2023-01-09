c77d9092ad735dfe99014fb4cfa1d6f136ee9b5c
Miftahul Anwar <miftahul.anwar@zeniuseducation.com>
2023-01-09 11:35:07 +0700

    remove body key

--- netlify/functions/identity-login.js
+++ netlify/functions/identity-login.js
@@ -1,4 +1,4 @@
-const netlifyIdentity = require("netlify-identity-widget");
+// const netlifyIdentity = require("netlify-identity-widget");
 
 exports.handler = async function (event, context) {
   // const identity = await netlifyIdentity.currentUser();
@@ -6,14 +6,13 @@ exports.handler = async function (event, context) {
   console.log("user:", user);
   console.log("identity:", identity);
 
-  // if (!identity || !identity.id) {
-  //   // User is not authenticated, return an error
-  //   console.log("unknown identity");
-  //   return {
-  //     statusCode: 401,
-  //     body: "Unauthorized",
-  //   };
-  // }
+  if (!identity || !identity.id) {
+    // User is not authenticated, return an error
+    console.log("unknown identity");
+    return {
+      statusCode: 401,
+    };
+  }
   // User is authenticated, proceed with login
   // Implement your login logic here
   console.log("hello");
