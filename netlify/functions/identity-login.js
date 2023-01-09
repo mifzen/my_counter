88a5f0310fc792e2eaed51e025441e44818e35f2
Miftahul Anwar <miftahul.anwar@zeniuseducation.com>
2023-01-09 10:50:48 +0700

    remove response

--- netlify/functions/identity-login.js
+++ netlify/functions/identity-login.js
@@ -4,14 +4,16 @@ exports.handler = async function (event, context) {
   // const identity = await netlifyIdentity.currentUser();
   const { identity, user } = context.clientContext;
   console.log("user:", user);
-  if (!identity || !identity.id) {
-    // User is not authenticated, return an error
-    console.log("unknown identity");
-    return {
-      statusCode: 401,
-      body: "Unauthorized",
-    };
-  }
+  console.log("identity:", identity);
+
+  // if (!identity || !identity.id) {
+  //   // User is not authenticated, return an error
+  //   console.log("unknown identity");
+  //   return {
+  //     statusCode: 401,
+  //     body: "Unauthorized",
+  //   };
+  // }
   // User is authenticated, proceed with login
   // Implement your login logic here
   console.log("hello");
